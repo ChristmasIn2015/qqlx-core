499963e9590c5e2b420175325b6e907605b4efa3
huangwenqiang <wqao1023@qq.com>
2023-01-14 10:03:25 +0800

    fixbugs

--- user/dto.ts
+++ user/dto.ts
@@ -9,7 +9,7 @@ export type postUserWeChatDto = { wechatResponseCode: string };
 export type postUserWeChatRes = { authorization: string };
 
 export type getUserDto = { MongodbQuery?: MongodbQuery<null>; ids?: ObjectId[] };
-export type getUserDtoRes = MongodbQuery<User>;
+export type getUserRes = MongodbQuery<User>;
 
 export type patchUserDto = User;
-export type patchUserDtoRes = User;
+export type patchUserRes = User;
