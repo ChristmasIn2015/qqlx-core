e42896e1133cf948d9c3886a4029719f5663d6eb
huangwenqiang <wqao1023@qq.com>
2023-03-08 21:58:33 +0800

    update

--- dto/brand/configCorp.ts
+++ dto/brand/configCorp.ts
@@ -2,9 +2,9 @@ import { MongodbPage, MongodbPageRes } from "../../utils/database";
 
 import type { ConfigCorp } from "../../schema/brand/configCorp";
 
-export const PATH_CONFIG_CORP = "/qqlx/corp/announce";
-export type postConfigCorp = ConfigCorp;
-export type postAnnounceRes = null;
+export const PATH_CONFIG_CORP = "/qqlx/corp/config";
+export type postConfigCorpDto = ConfigCorp;
+export type postConfigCorpRes = null;
 
-export type getAnnounceDto = null;
-export type getAnnounceRes = ConfigCorp;
+export type getConfigCorpDto = null;
+export type getConfigCorpRes = ConfigCorp;
