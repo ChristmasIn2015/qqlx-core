4bb1ab90d0061902c8b1bff2e99798e5a2c3a63f
huangwenqiang <974879409@qq.com>
2023-03-22 17:13:42 +0800

    update

--- dto/oa/lineAccess.ts
+++ dto/oa/lineAccess.ts
@@ -1,8 +1,9 @@
 import type { AccessJoined } from "../../dto/oa/access";
+import type { Access } from "../../schema/oa/access";
 import type { LineAccess } from "../../schema/oa/lineAccess";
 
 export const PATH_OA_LINEACCESS = "/qqlx/oa/line/access";
-export type postLineAccessDto = LineAccess;
+export type postLineAccessDto = Access[];
 export type postLineAccessRes = null;
 
 export type deleteLineAccessDto = { entityId: string };
