46e95f6cca0aae7d1a9024d61a49cc9c75a80606
huangwenqiang <974879409@qq.com>
2023-04-19 22:27:27 +0800

    1.0.44

--- dist/src/market/project.schema.d.ts
+++ dist/src/market/project.schema.d.ts
@@ -1,10 +1,10 @@
 import { EnumMapOption } from "qqlx-cdk";
 /** 项目分类 */
 export declare enum ENUM_PROJECT {
-    /** 清泉流响 钢材·开单办公室 */
+    /** 清泉流响 钢材·钢材办公室 */
     KDBGS = 1000,
     /** 清泉流响  */
-    OA = 2000
+    OA = 2000,
 }
 declare const MAP_ENUM_PROJECT: Map<ENUM_PROJECT, EnumMapOption>;
 export { MAP_ENUM_PROJECT };
