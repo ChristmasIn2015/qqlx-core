14227551df68eba892693ca368cea008be9a1958
huangwenqiang <974879409@qq.com>
2023-05-07 18:11:14 +0800

    1.0.69

--- dist/src/wmss/analysis.dto.d.ts
+++ dist/src/wmss/analysis.dto.d.ts
@@ -1,7 +1,6 @@
-import { Page, PageRes, MongodbSort } from "qqlx-cdk";
+import { MongodbSort } from "qqlx-cdk";
 import type { Contact } from "../brand/contact.schema";
 import type { ENUM_ORDER } from "./order.schema";
-import { ContactAnalysis } from "./analysis.schema";
 export declare const PATH_ORDER_ANALYSIS = "/qqlx/wmss/order/analysis";
 export type OrderAnalysis = {
     type: ENUM_ORDER;
@@ -17,16 +16,24 @@ export type OrderAnalysis = {
 export type getOrderAnalysisDto = OrderAnalysis;
 export type getOrderAnalysisRes = OrderAnalysis;
 export declare const PATH_CONTACT_ANALYSIS = "/qqlx/wmss/contact/analysis";
-export type ContactAnalysisJoined = ContactAnalysis & {
-    joinContact?: Contact;
-};
 export type getContactAnalysisDto = {
-    page: Page;
-    search: ContactAnalysis;
-    sortKey?: string;
-    sortValue?: MongodbSort;
+    type: ENUM_ORDER;
+    startTime: number;
+    endTime: number;
+    sortKey: string;
+    sortValue: MongodbSort;
+    contactId?: string;
 };
-export type getContactAnalysisRes = PageRes<ContactAnalysisJoined>;
+export type getContactAnalysisRes = {
+    type: ENUM_ORDER;
+    contactId: string;
+    joinContact?: Contact;
+    /** 订单数量 */
+    count: number;
+    amount: number;
+    amountBookOfOrder: number;
+    amountBookOfOrderRest: number;
+}[];
 export declare const PATH_SKU_ANALYSIS = "/qqlx/wmss/sku/analysis";
 export type getSkuAnalysisDto = {
     startTime: number;
--- src/wmss/analysis.dto.ts
+++ src/wmss/analysis.dto.ts
@@ -3,7 +3,6 @@ import { Page, PageRes, MongodbSort } from "qqlx-cdk";
 import type { UserInfo } from "../user/user.dto";
 import type { Contact } from "../brand/contact.schema";
 import type { Order, ENUM_ORDER } from "./order.schema";
-import { ContactAnalysis } from "./analysis.schema";
 
 export const PATH_ORDER_ANALYSIS = "/qqlx/wmss/order/analysis";
 export type OrderAnalysis = {
@@ -21,17 +20,26 @@ export type getOrderAnalysisDto = OrderAnalysis;
 export type getOrderAnalysisRes = OrderAnalysis;
 
 export const PATH_CONTACT_ANALYSIS = "/qqlx/wmss/contact/analysis";
-export type ContactAnalysisJoined = ContactAnalysis & {
-    joinContact?: Contact;
-};
 export type getContactAnalysisDto = {
-    page: Page;
-    search: ContactAnalysis;
+    type: ENUM_ORDER;
+    startTime: number;
+    endTime: number;
+    sortKey: string;
+    sortValue: MongodbSort;
 
-    sortKey?: string;
-    sortValue?: MongodbSort;
+    contactId?: string;
 };
-export type getContactAnalysisRes = PageRes<ContactAnalysisJoined>;
+export type getContactAnalysisRes = {
+    type: ENUM_ORDER;
+    contactId: string;
+    joinContact?: Contact;
+
+    /** 订单数量 */
+    count: number;
+    amount: number;
+    amountBookOfOrder: number;
+    amountBookOfOrderRest: number;
+}[];
 
 export const PATH_SKU_ANALYSIS = "/qqlx/wmss/sku/analysis";
 export type getSkuAnalysisDto = {
