52bc4ae9cdcc32a8db6c78bee02b5bedf2e1e4dd
huangwenqiang <huangwenqiang@shoptop.com>
2023-11-28 18:15:15 +0800

    updates

--- src/pond-log/dto.ts
+++ src/pond-log/dto.ts
@@ -1,5 +1,5 @@
 import { PondLog } from "./schema";
 
-export const RELATIONS_NAME = 'pond_log'
+export const RELATIONS_POND_LOG_NAME = 'pond_log'
 export type postPondLogDto = { schema: PondLog }
 export type postPondLogRes = null
