87f74205fbaa4680a40c63b0f89f76e1f160b87b
huangwenqiang <huangwenqiang@shoptop.com>
2023-12-12 19:53:12 +0800

    updates

--- src/stream-user-role/dto.ts
+++ src/stream-user-role/dto.ts
@@ -1,5 +1,9 @@
-import { INTEGER_PG } from "../_/db.pg";
-import { DraftNode } from "./schema";
+import { UserRole } from "./schema";
+
+export enum ENUM_ROLE_SCOPE {
+    /** 蓝图系统 */
+    RIVER_DRAFT = 1000
+}
 
 export enum ENUM_ROLE_CONTENT {
     /** 所有权 */
@@ -10,7 +14,5 @@ export enum ENUM_ROLE_CONTENT {
     WRITE
 }
 
-/** 内部系统枚举 */
-export enum ENUM_ROLE_SCOPE {
-    RIVER_DRAFT = 1000
-}
+export const DROPLET_STREAM_USER_ROLE = "stream:user:role";
+export const PATH_STREAM_USER_ROLE = "/stream/user/role";
