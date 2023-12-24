f7ff2babd6d1f59e88cecc71caca78f183ffe39f
huangwenqiang <974879409@qq.com>
2023-12-24 09:30:57 +0800

    updates

--- src/stream-access/dto.ts
+++ src/stream-access/dto.ts
@@ -1,32 +1,32 @@
-import type { PondIdentityGroup, PondIdentity, PondIdentityGroupUser } from "./schema";
+import type { PondAccessGroup, PondAccess, PondAccessGroupUser } from "./schema";
 import type { INTEGER_PG, PgBaseSchema, VARCHAR255_PG, VARCHAR50_PG } from "../../_/db.pg";
 
-export const PATH_POND_IDENTITY_GROUP = "/pond/identity/group";
+export const PATH_POND_ACCESS_GROUP = "/pond/access/group";
 /** 每个业务系统必须保证，权限范围中的资源所有权，必须和提交者一致
  * @scope 例如 brand:corp:10001:sale 主体的所有者必须是提交者
  */
-export type postPondIdentityGroupDto = { schema: PondIdentityGroup };
-export type postPondIdentityGroupRes = null;
-export type getPondIdentityGroupDto = { scope: VARCHAR255_PG };
-export type getPondIdentityGroupRes = PondIdentityGroup[];
-export type putPondIdentityGroupDto = { entity: PondIdentityGroup };
-export type putPondIdentityGroupRes = null;
-export type deletePondIdentityGroupDto = { id: INTEGER_PG };
-export type deletePondIdentityGroupRes = null;
+export type postPondAccessGroupDto = { schema: PondAccessGroup };
+export type postPondAccessGroupRes = null;
+export type getPondAccessGroupDto = { scope: VARCHAR255_PG };
+export type getPondAccessGroupRes = PondAccessGroup[];
+export type putPondAccessGroupDto = { entity: PondAccessGroup };
+export type putPondAccessGroupRes = null;
+export type deletePondAccessGroupDto = { id: INTEGER_PG };
+export type deletePondAccessGroupRes = null;
 
-export const PATH_POND_IDENTITY = "/pond/identity";
-export const DROPLET_POND_IDENTITY = PATH_POND_IDENTITY.replace(/\//, "").replace(/\//, ":");
+export const PATH_POND_ACCESS = "/pond/access";
+export const DROPLET_POND_ACCESS = PATH_POND_ACCESS.replace(/\//, "").replace(/\//, ":");
 /** 资源范围中的所有者才能添加/删除具体的权限明细 */
-export type postPondIdentityDto = { schema: PondIdentity };
-export type postPondIdentityRes = null;
-export type deletePondIdentityDto = { id: INTEGER_PG };
-export type deletePondIdentityRes = null;
+export type postPondAccessDto = { schema: PondAccess };
+export type postPondAccessRes = null;
+export type deletePondAccessDto = { id: INTEGER_PG };
+export type deletePondAccessRes = null;
 
-export const PATH_POND_IDENTITY_GROUP_USER = "/pond/identity/group/user";
+export const PATH_POND_ACCESS_GROUP_USER = "/pond/access/group/user";
 /** 资源范围中的所有者才能添加/删除具体的外部访客
  * @important 一般会把这个实体交给各个业务系统，业务系统再通过身份组微服务去判断发起者是否有权限进一步在业务系统中继续操作
  */
-export type postPondIdentityGroupUserDto = { schema: PondIdentityGroupUser };
-export type postPondIdentityGroupUserRes = null;
-export type deletePondIdentityGroupUserDto = { id: INTEGER_PG };
-export type deletePondIdentityGroupUserRes = null;
+export type postPondAccessGroupUserDto = { schema: PondAccessGroupUser };
+export type postPondAccessGroupUserRes = null;
+export type deletePondAccessGroupUserDto = { id: INTEGER_PG };
+export type deletePondAccessGroupUserRes = null;
--- src/stream-access/schema.ts
+++ src/stream-access/schema.ts
@@ -1,7 +1,7 @@
 import type { PgBaseSchema, INTEGER_PG, VARCHAR50_PG, VARCHAR_PG, VARCHAR255_PG } from "../../_/db.pg";
 import type { UserInfo } from "../stream-user/dto";
 
-export enum ENUM_POND_IDENTITY {
+export enum ENUM_POND_ACCESS {
     /** 读取 */
     READONLY = 1001,
     /** 写入/修改 */
@@ -14,33 +14,33 @@ export enum ENUM_POND_IDENTITY {
  * @uid 持有者
  * @scope 指出资源范围，如 brand:corp:10001:sale
  */
-export type PondIdentityGroup = {
+export type PondAccessGroup = {
     uid: VARCHAR50_PG;
     name: VARCHAR50_PG;
     desc: VARCHAR255_PG;
     scope: VARCHAR255_PG;
 
-    joinIdentities?: PondIdentity[];
+    joinIdentities?: PondAccess[];
     joinUserInfo?: UserInfo[];
 } & PgBaseSchema;
-export const RELATIONS_POND_IDENTITY_GROUP = "pond_identity_group";
+export const RELATIONS_POND_ACCESS_GROUP = "pond_access_group";
 
 /** 权限明细
  * @gid 身份组
  * @type 1.允许读取 2.允许添加/修改 3.允许删除
  */
-export type PondIdentity = {
+export type PondAccess = {
     gid: INTEGER_PG;
-    type: ENUM_POND_IDENTITY;
+    type: ENUM_POND_ACCESS;
 } & PgBaseSchema;
-export const RELATIONS_POND_IDENTITY = "pond_identity";
+export const RELATIONS_POND_ACCESS = "pond_access";
 
 /** 外部访客持有的身份
  * @uid 外部访客
- * @identityId 各个业务系统需要判断访问者是否具有某种权限的身份
+ * @accessId 各个业务系统需要判断访问者是否具有某种权限的身份
  */
-export type PondIdentityGroupUser = {
+export type PondAccessGroupUser = {
     gid: INTEGER_PG;
     uid: VARCHAR50_PG;
 } & PgBaseSchema;
-export const RELATIONS_POND_IDENTITY_GROUP_USER = "pond_identity_group_user";
+export const RELATIONS_POND_ACCESS_GROUP_USER = "pond_access_group_user";
