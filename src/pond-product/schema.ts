525b686e5f7f5b405d6d1508c34b49675d800d1b
huangwenqiang <974879409@qq.com>
2023-12-27 23:29:48 +0800

    update

--- src/pond-product/schema.ts
+++ src/pond-product/schema.ts
@@ -1,30 +1,36 @@
 import type { PgBaseSchema, INTEGER_PG, VARCHAR50_PG, VARCHAR_PG, SMALLINT_PG, VARCHAR255_PG } from "../../_/db.pg";
 
-/** 产品分组
-*/
-export type SpuGroup = {
+/** 商品分组
+ */
+export type SkuTemplateGroup = {
     uid: VARCHAR50_PG;
     name: VARCHAR50_PG;
     desc: VARCHAR255_PG;
-    remark: VARCHAR255_PG;
 } & PgBaseSchema;
 
-export type SpuGroupRelation = {
-    oid: INTEGER_PG;
+export type SkuTemplateRelation = {
+    uid: VARCHAR50_PG;
     gid: INTEGER_PG;
+    skuId: INTEGER_PG;
 
-    joinSpu?: Spu
-    joinGroup?: SpuGroup
+    joinSkuTemplate?: SkuTemplate;
 } & PgBaseSchema;
 
 /** 产品
-*/
-export type Spu = {
+ */
+export type SkuTemplate = {
     uid: VARCHAR50_PG;
+
     name: VARCHAR50_PG;
-    desc: VARCHAR255_PG;
-    remark: VARCHAR255_PG;
+    norm: VARCHAR255_PG;
 
-    md5: VARCHAR50_PG;
+    count: INTEGER_PG;
+
+    /** 唯一识别库存，根据名称、规格、资产、其他标签组成 */
+    tonId: VARCHAR50_PG;
+
+    /** 吨位：1吨 = 1000kg = 1,000,000g */
+    ton: INTEGER_PG;
+
+    price: INTEGER_PG;
 } & PgBaseSchema;
-
